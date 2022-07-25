48a748804d5e13742b3a34f10c6bae215aad441a
toma <tomajohn316@yahoo.com>
2022-07-25 16:30:09 +0100

    Add logo image and Navlink

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => (
+  <header>
+    <nav>
+      <NavLink />
+    </nav>
+  </header>
+);
+
+export default NavBar;
