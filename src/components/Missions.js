5e09bf0a0dbef912bff9595fb26390f86daf5323
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-27 11:38:51 +0100

    Added the mission components content

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { retrieveMissions } from '../Redux/missions/missions';
+import Table from 'react-bootstrap/Table';
+import Mission from './Mission';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
@@ -12,7 +14,23 @@ const Missions = () => {
     }
   }, []);
 
-  return <div>THis is Mission section</div>;
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th className="hide">Hello</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          missions.map((mission) => <Mission key={mission.mission_id} mission={mission} />)
+          }
+      </tbody>
+    </Table>
+  );
 };
 
 export default Missions;
